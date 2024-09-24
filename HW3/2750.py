eea6154d7d94aa8bee06971c332d28aaa89cfc5d
Leekwansuk <lks000203@gmail.com>
2024-09-24 17:07:46 +0900

    HW3 완성

--- HW3/2750.py
+++ HW3/2750.py
@@ -1,7 +1,9 @@
-a = set() ##set 함수를 사용하는 이유는 중복 거를 수 있으므로
+a = []
 b = int(input())
-for i in range(b): ## 새로 알게 된 tip : 여러줄을 넣을 땐 b로 넣기
-    a.add(int(input())) ## tip : 순수 input 그대로 넣어야 함 b를 넣으면 덩어리로 들어가기 때문에 인식을 못함
-c = list(a)
-for k in range(len(c)):
-    print(c[k])
+
+for i in range (b):
+    c = int(input())
+    a.append(c)
+a.sort()
+for k in range(b):
+    print(a[k])
